94bf56aaccae9b5c383a895eb09408f98917d7d4
sauravghule <saurav.ghule.za05@gmail.com>
2026-08-30 20:31:14 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-export default defineConfig({ plugins: [react()], base: process.env.GITHUB_ACTIONS ? '/ca-final-tracker/' : '/' });
+
+export default defineConfig({
+  plugins: [react()],
+  base: process.env.GITHUB_ACTIONS
+    ? '/CA-Final-Exam-Tracker/'
+    : '/',
+});
